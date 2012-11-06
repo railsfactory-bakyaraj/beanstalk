76dcf490d02dab9481155cf0cb95515f8e8a15f5
Bakyaraj <bakyaraj@railsfactory.org>
2012-11-06 13:03:35 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beanstalk::Application.config.secret_token = '7af789ed4bf23ed593cd882608ac5b2bdca8fc62148b6f62b674a1e5c44a25b4066397ee1f31e85df923d1e9671845d0b75ece7f791a2be4a60bb31eedecafd5'
